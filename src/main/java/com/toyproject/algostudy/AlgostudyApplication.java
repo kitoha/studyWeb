2bf84639fc75d103aaf581be87b36fb6e25fec42
kitoha <kitoha@daum.net>
2020-02-29 03:47:32 +0900

    start project

---
+++ src/main/java/com/toyproject/algostudy/AlgostudyApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.algostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgostudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgostudyApplication.class, args);
+	}
+
+}
