219625a8277a44d15922eb6174c343e181faa5a5
kitoha <kitoha@daum.net>
2020-03-10 15:42:02 +0900

    add spring security

--- src/main/java/com/toyproject/algostudy/HelloController.java
+++ src/main/java/com/toyproject/algostudy/HelloController.java
@@ -1,18 +1,62 @@
 package com.toyproject.algostudy;
 
+import com.toyproject.algostudy.dto.auth.SessionUser;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@Slf4j
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
 
+    private final HttpSession httpSession;
+
     @GetMapping("/")
     public String root(){
+
         return "logInSuccess";
+    }
+
+    @GetMapping("/test")
+    public String root2(){
+        httpSession.removeAttribute("user");
+        return "logInSuccess";
+    }
+
+    @GetMapping("/test3")
+    public String root3(){
+        SessionUser user =(SessionUser)httpSession.getAttribute("user");
+
+        if(user!=null){
+            return "I have Session";
+        }
+        return "helllo";
+    }
+
+    @GetMapping("/logout")
+    public String logOut(HttpServletRequest request, HttpServletResponse response){
+
+        for(Cookie cookie: request.getCookies()){
+            log.info(cookie.getName() + " "+ cookie.getPath());
+        }
+        return "success";
     }
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello";
+
+        SessionUser user =(SessionUser)httpSession.getAttribute("user");
+
+        if(user!=null){
+            return "I have Session";
+        }
+        return "helllo";
     }
 }
