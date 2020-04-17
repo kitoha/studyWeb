c21e59b0f6d625e0368ddb1fd5ec752b0e392a03
kitoha <kitoha@daum.net>
2020-04-17 18:10:57 +0900

    modify security

--- src/main/java/com/toyproject/algostudy/config/auth/SecurityConfig.java
+++ src/main/java/com/toyproject/algostudy/config/auth/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().disable();
 
         http.authorizeRequests()
-                .antMatchers("/","/favicon.ico","/css/**",".jpg",".png",".js","/h2-console/**","/api/v1/auth/**","api/v1/participatingGroup/**")
+                .antMatchers("/","/favicon.ico","/css/**",".jpg",".png",".js","/h2-console/**","/api/v1/auth/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated();
